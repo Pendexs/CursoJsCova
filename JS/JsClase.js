85d5e440601ee07470b3ddaefe4822ae586eb2be
Pendexs <87399596+Pendexs@users.noreply.github.com>
2022-09-12 22:20:04 -0300

    Fetch

--- JS/JsClase.js
+++ JS/JsClase.js
@@ -1,3 +1,9 @@
+
+fetch('https://criptoya.com/api/dolar')
+    .then( response => response.json() )
+    .then (({oficial}) => {
+
+
 const carrito = []  // ARRAY carrito
 class producto {   // CREADOR de productos/objetos
     
@@ -9,7 +15,7 @@ class producto {   // CREADOR de productos/objetos
       this.imagen = imagen;
     }
  }
-  
+
   const diablo = new producto(1, "Diablo", 800, 1, "https://http2.mlstatic.com/D_NQ_NP_2X_929142-MLA45343464483_032021-F.webp"); 
   const verde = new producto(2, "Verde", 400, 1, "https://images.cults3d.com/Sc4hyz-V2GaCf_iNb9xvcnkcGvY=/516x516/https://files.cults3d.com/uploaders/18131720/illustration-file/23e0b840-2e1c-4537-9f2e-0db7c42d1159/grinder.jpg#1") ;
   const dorado = new producto(3, "Dorado", 300, 1, "https://images.squarespace-cdn.com/content/v1/59d5f200bebafbe0830d4c9b/1562664238015-SLASGUM3BP5O28MA4AAW/Grinder-solid-8-gold-open.jpg?format=1000w");
@@ -26,15 +32,56 @@ productos.forEach((producto) => {
         <div class="card-body">
             <h3>${producto.nombre}</h3>
             <img class = "trein" src=${producto.imagen}  alt="">
-            <p class="card-text">$ ${producto.precio}</p>
+            <p class="card-text convertir" id="dolar${producto.id}">$ ${producto.precio}</p>
             <a href="#" id="boton${producto.id}">Comprar</a>
         </div>
     </article>
-    `
+    `    
 
+    console.log(producto.precio)
     
 }
 )
+
+const dolarizar = document.getElementById("dolarizar") // Para convertir precios a dolar oficial
+
+dolarizar.addEventListener('click', () => {
+
+  productos.forEach((producto) => {
+    producto.precio = (producto.precio/oficial).toFixed(2)
+
+    console.log(producto.precio)
+    
+    let preciodolar = `USD ${producto.precio}
+    `
+    let dolar =document.getElementById(`dolar${producto.id}`)
+    dolar.innerHTML= preciodolar
+
+  })
+
+  })
+
+
+  
+  const pecificar = document.getElementById("pesificar") // Convertir precios a Pesos
+
+
+  pecificar.addEventListener('click', () => {
+
+  productos.forEach((producto) => {
+    producto.precio = (producto.precio*oficial).toFixed(0 )
+
+    console.log(producto.precio)
+    
+    let preciodolar = `$ ${producto.precio}
+    `
+    let dolar =document.getElementById(`dolar${producto.id}`)
+    dolar.innerHTML= preciodolar
+console.log("pesos")
+  })
+
+  })
+
 
 productos.forEach((producto) => {                   //Uso un forEach para los botones de compra
   let botonCompra = document.getElementById(`boton${producto.id}`)
@@ -55,6 +102,7 @@ productos.forEach((producto) => {                   //Uso un forEach para los bo
       }).showToast();
   }
   )
+    
 })
 
 //FUNCIONES
@@ -97,6 +145,7 @@ function agregarCarrito (idProducto) // Funcion para agregar al carrito
     </article>
     `}
 
+
   const obtenerTotal = (productosArray) => {     //Calculo el Total $$ multiplico cantidad por precio de cada objeto en el Array del carrito
       let total = 0;
     productosArray.forEach((producto) => {
@@ -116,11 +165,35 @@ let lista = document.getElementById("precioFinal");
 lista.innerHTML = "El total es $" + totalCarrito;
 
 
+dolarizar.addEventListener('click', () => {     // Dolarizo el total
+  console.log("dolarrrr")
+
+  productos.forEach((producto) => {
+  lista.innerHTML = "El total es USD " + totalCarrito;
+
+  let dolarPrecio = document.getElementById(`cantidad${seleccion.id}`);
+
+  dolarPrecio.innerHTML =`USD ${seleccion.precio} x${seleccion.cantidad}`
+
+  })
+
+  })
+
+
+
 let carritoJSON = JSON.stringify (carrito)  // Agrego al almacenamiento local el carrito en forma de String y el total de la compra
   
 localStorage.setItem("Carrito", carritoJSON )
 localStorage.setItem("Total", obtenerTotal(carrito))
-}
+
+}  //Fin Agregar carrito
     
 console.log(carrito)
+
+
+})
+
+
+
+
 
